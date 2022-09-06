12778e59b6f3e15e5b72bea3d73572e008867e0c
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2022-09-06 13:19:52 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labb_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Slumpa(object sender, RoutedEventArgs e)
+        {
+            Random gen = new Random();
+            slumpTal.Text = gen.Next(1, 101).ToString();
+        }
+    }
+}

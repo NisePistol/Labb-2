b90db13ba22f951de17165577c4fbdb2580ceb57
Adrian Herrera Algotsson <blazer1458@gmail.com>
2022-11-11 12:14:04 +0100

    X

--- obj/Debug/net6.0-windows/MainWindow.g.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.cs
@@ -60,7 +60,7 @@ namespace Labb_2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Labb-2;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Labb-2;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
